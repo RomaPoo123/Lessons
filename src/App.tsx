17c654d1cb7356e79da43769cdb544c7315c1d69
Roman <113453538+RomaPoo123@users.noreply.github.com>
2024-08-12 20:02:12 +0300

    UseState

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import { MicroTasks } from './microtasks/MicroTasks';
+import { OnnOff } from './06-useState/06';
 
 function App() {
   return (
     <div className="App">
-      <MicroTasks />
+      {/* <MicroTasks /> */}
+      <OnnOff />
     </div>
   );
 }
