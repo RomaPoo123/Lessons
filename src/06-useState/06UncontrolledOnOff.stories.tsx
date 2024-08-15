8e7ef2135abd8ffc4a5b6de28079c4d227bd4181
Roman <113453538+RomaPoo123@users.noreply.github.com>
2024-08-15 19:16:53 +0300

    storybook_1

---
+++ src/06-useState/06UncontrolledOnOff.stories.tsx
@@ -0,0 +1,142 @@
+import type { Meta, StoryObj } from "@storybook/react/*";
+import { useState } from "react";
+import { OnnOff } from "./06OnOff";
+import { UncontrolledOnnOff } from "./06UncontrolledOnOff";
+import { Button } from "../microtasks/lesson06/Button";
+import { action } from "@storybook/addon-actions"
+
+const meta: Meta<typeof UncontrolledOnnOff> = {
+    component: UncontrolledOnnOff,
+}
+
+export default meta;
+
+const onChangeHandler = action("onChange")
+
+type Story = StoryObj<typeof OnnOff>
+export const FirstStory: Story = {
+    args: {
+        value: false,
+        onChange: () => { },
+    }
+}
+
+
+
+export const OnUncontrolledOnnOff = () => {
+    let [value, setValue] = useState<boolean>(true)
+
+    const onStyle = {
+        width: "30px",
+        height: "20px",
+        border: "1px solid black",
+        display: "inline-block",
+        padding: "2px",
+        backgroundColor: value ? "green" : "white",
+        borderRadius: "2px",
+    }
+    const offStyle = {
+        width: "30px",
+        height: "20px",
+        border: "1px solid black",
+        display: "inline-block",
+        marginLeft: "2px",
+        padding: "2px",
+        backgroundColor: (!value) ? "red" : "white",
+        borderRadius: "2px",
+    }
+    const indicatorStyle = {
+        width: "10px",
+        height: "10px",
+        borderRadius: "5px",
+        border: "1px solid black",
+        display: "inline-block",
+        marginLeft: "5px",
+        backgroundColor: value ? "green" : "red",
+    }
+    return (
+        <div>
+            <button onClick={onChangeHandler} style={onStyle}>On</button>
+            <button onClick={onChangeHandler} style={offStyle}>Off</button>
+            <div style={indicatorStyle}></div>
+        </div>
+    )
+}
+export const OffUncontrolledOnnOff = () => {
+    let [value, setValue] = useState<boolean>(false)
+
+    const onStyle = {
+        width: "30px",
+        height: "20px",
+        border: "1px solid black",
+        display: "inline-block",
+        padding: "2px",
+        backgroundColor: value ? "green" : "white",
+        borderRadius: "2px",
+    }
+    const offStyle = {
+        width: "30px",
+        height: "20px",
+        border: "1px solid black",
+        display: "inline-block",
+        marginLeft: "2px",
+        padding: "2px",
+        backgroundColor: (!value) ? "red" : "white",
+        borderRadius: "2px",
+    }
+    const indicatorStyle = {
+        width: "10px",
+        height: "10px",
+        borderRadius: "5px",
+        border: "1px solid black",
+        display: "inline-block",
+        marginLeft: "5px",
+        backgroundColor: value ? "green" : "red",
+    }
+    return (
+        <div>
+            <button onClick={() => { }} style={onStyle}>On</button>
+            <button onClick={() => { }} style={offStyle}>Off</button>
+            <div style={indicatorStyle}></div>
+        </div>
+    )
+}
+export const WarkingUncontrolledOnnOff = () => {
+    let [value, setValue] = useState<boolean>(false)
+
+    const onStyle = {
+        width: "30px",
+        height: "20px",
+        border: "1px solid black",
+        display: "inline-block",
+        padding: "2px",
+        backgroundColor: value ? "green" : "white",
+        borderRadius: "2px",
+    }
+    const offStyle = {
+        width: "30px",
+        height: "20px",
+        border: "1px solid black",
+        display: "inline-block",
+        marginLeft: "2px",
+        padding: "2px",
+        backgroundColor: (!value) ? "red" : "white",
+        borderRadius: "2px",
+    }
+    const indicatorStyle = {
+        width: "10px",
+        height: "10px",
+        borderRadius: "5px",
+        border: "1px solid black",
+        display: "inline-block",
+        marginLeft: "5px",
+        backgroundColor: value ? "green" : "red",
+    }
+    return (
+        <div>
+            <button onClick={() => setValue(true)} style={onStyle}>On</button>
+            <button onClick={() => setValue(false)} style={offStyle}>Off</button>
+            <div style={indicatorStyle}></div>
+        </div>
+    )
+}
